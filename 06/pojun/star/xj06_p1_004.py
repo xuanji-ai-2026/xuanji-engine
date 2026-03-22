9f67c0b91024b66051524d4a2adef84b495a61d3
AI-Employee-136 <136@xuanji.ai>
2026-03-22 12:17:34 +0800

    feat(06_pojun_star): [P1] 电话外呼插件 (136)

--- 06/pojun/star/xj06_p1_004.py
+++ 06/pojun/star/xj06_p1_004.py
@@ -3,7 +3,7 @@
 版本: v2.0
 负责人: 136
 任务ID: XJ06-P1-004
-创建时间: 2026-03-22 11:30
+创建时间: 2026-03-22 12:17
 
 功能: 外呼
 """
@@ -16,10 +16,10 @@ class 电话外呼插件:
     外呼
     """
     
-    def __init__(self) -> None -> None:
+    def __init__(self):
         pass
     
-    def async run(self) -> Dict:
+    async def run(self) -> Dict:
         """运行"""
         return {}
 
