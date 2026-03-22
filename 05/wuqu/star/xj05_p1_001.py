27f0791542143d541fa077ab22d0a0d9f68192ac
AI-Employee-127 <127@xuanji.ai>
2026-03-22 12:17:08 +0800

    feat(05_wuqu_star): [P1] 插件基类与接口 (127)

--- 05/wuqu/star/xj05_p1_001.py
+++ 05/wuqu/star/xj05_p1_001.py
@@ -3,7 +3,7 @@
 版本: v2.0
 负责人: 127
 任务ID: XJ05-P1-001
-创建时间: 2026-03-22 11:29
+创建时间: 2026-03-22 12:17
 
 功能: 接口
 """
@@ -16,10 +16,10 @@ class 插件基类与接口:
     接口
     """
     
-    def __init__(self) -> None -> None:
+    def __init__(self):
         pass
     
-    def async run(self) -> Dict:
+    async def run(self) -> Dict:
         """运行"""
         return {}
 
