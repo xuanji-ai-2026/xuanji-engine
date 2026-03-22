c31e6a3b4b273a8d5cfd9bca5a0e36f352d35b34
AI-Employee-161 <161@xuanji.ai>
2026-03-22 12:18:20 +0800

    feat(10_fubi_star): [P1] API网关 (161)

--- 10/fubi/star/xj10_p1_001.py
+++ 10/fubi/star/xj10_p1_001.py
@@ -3,7 +3,7 @@ API网关
 版本: v2.0
 负责人: 161
 任务ID: XJ10-P1-001
-创建时间: 2026-03-22 11:31
+创建时间: 2026-03-22 12:18
 
 功能: 网关
 """
@@ -16,10 +16,10 @@ class API网关:
     网关
     """
     
-    def __init__(self) -> None -> None:
+    def __init__(self):
         pass
     
-    def async run(self) -> Dict:
+    async def run(self) -> Dict:
         """运行"""
         return {}
 
