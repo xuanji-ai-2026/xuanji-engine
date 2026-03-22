2906c116064d274de8c2569e6520549f420ea9ff
AI-Employee-128 <128@xuanji.ai>
2026-03-22 12:17:11 +0800

    feat(05_wuqu_star): [P1] 插件注册中心 (128)

--- 05/wuqu/star/xj05_p1_002.py
+++ 05/wuqu/star/xj05_p1_002.py
@@ -3,7 +3,7 @@
 版本: v2.0
 负责人: 128
 任务ID: XJ05-P1-002
-创建时间: 2026-03-22 11:29
+创建时间: 2026-03-22 12:17
 
 功能: 注册
 """
@@ -16,10 +16,10 @@ class 插件注册中心:
     注册
     """
     
-    def __init__(self) -> None -> None:
+    def __init__(self):
         pass
     
-    def async run(self) -> Dict:
+    async def run(self) -> Dict:
         """运行"""
         return {}
 
